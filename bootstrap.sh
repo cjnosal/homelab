23476edd3d20672a24dafa2270a8edbbbeb18d4f
Conor Nosal <c.nosal@gmail.com>
2024-02-03 13:36:27 -0500

    Add logs to bootstrap and reset scripts
    init.sh output is saved to init.log

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo prepare workspace
 ssh root@192.168.2.200 mkdir -p /root/workspace/
 scp -r ./cloudinit ./proxmox root@192.168.2.200:/root/workspace/
 
-ssh root@192.168.2.200 ./workspace/proxmox/init.sh
+echo initialize environment
+time ssh root@192.168.2.200 ./workspace/proxmox/init.sh | tee init.log
--- proxmox/reset.sh
+++ proxmox/reset.sh
@@ -3,8 +3,13 @@ set -eu
 
 truncate -s 0 /root/.ssh/known_hosts
 
+echo shutting down all vms
 ./workspace/proxmox/ids | grep -v 9000 | xargs -I{} -n1 qm shutdown {} -forceStop
+
+echo unlocking all vms
 ./workspace/proxmox/ids | grep -v 9000 | xargs -I{} -n1 qm unlock {}
+
+echo destroying all vms
 ./workspace/proxmox/ids | grep -v 9000 | xargs -I{} -n1 qm destroy {}
 
 rm -rf workspace/creds
