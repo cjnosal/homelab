9564d01caf047d8edc967ea547b02c11695339f5
Conor Nosal <c.nosal@gmail.com>
2024-04-13 16:58:17 -0400

    Allow gpg to overwrite apt keyrings with --yes

--- cloudinit/base/scripts/addcaddy.sh
+++ cloudinit/base/scripts/addcaddy.sh
@@ -6,7 +6,7 @@ help_this="install caddy"
 source /usr/local/include/argshelper
 parseargs $@
 
-curl -fSsL 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -fSsL 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --yes --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -fSsL 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 apt-get update
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --force-yes caddy
